a46c96da244c8614a69c642fdd71a22a3c8d99e2
Bob Vincent <bobvin@pillars.net>
2011-06-23 17:37:40 -0400

    More coder fixes.

--- uc_option_image.js
+++ uc_option_image.js
@@ -1,28 +1,30 @@
+(function ($) {
+
 var UCOI = UCOI || {};
 
 /**
  * Initialize.
  */
 UCOI.init = function() {
-  var size = Drupal.settings.UCOI.size;   
-  this.images = Drupal.settings.UCOI.images; 
+  var size = Drupal.settings.UCOI.size;
+  this.images = Drupal.settings.UCOI.images;
   this.effect = Drupal.settings.UCOI.effect;
   this.noimage = Drupal.settings.UCOI.noimage;
   this.attributes = Drupal.settings.UCOI.attributes;
   this.defaultSize = Drupal.settings.UCOI.default_size;
   this.nodeid = Drupal.settings.UCOI.nodeid;
-  
-  // Selects                                      
+
+  // Selects
   $('.add-to-cart select.form-select').change(function(){
     if (aid = UCOI.getAID(this)){
-      UCOI.switchImage(aid, this, size);  
+      UCOI.switchImage(aid, this, size);
     }
   });
-  
-  // Radios                                      
+
+  // Radios
   $('.add-to-cart .form-radios input').click(function(){
     if (aid = UCOI.getAID(this)){
-      UCOI.switchImage(aid, this, size);  
+      UCOI.switchImage(aid, this, size);
     }
   });
 };
@@ -32,20 +34,20 @@ UCOI.init = function() {
  */
 UCOI.switchImage = function(aid, input, size) {
   var nid = this.nodeid;
-  var oid = $(input).val(); 
+  var oid = $(input).val();
   var image =  $(':not(.uc-option-image-preloaded) > div.uc-option-image-block').children('img.uc-option-image');
-          
+
   // Make sure we have permission to switch this attribute
   if (this.attributes[aid] == 0){
     return;
   }
 
-  try {          
+  try {
     var images = this.images[nid][aid];
 
     if ((oid=="" || images[oid].derivative=="") && image) {
 	    parentImage = image[0].parentNode;
-	    parentImage.removeChild(image[0]);    
+	    parentImage.removeChild(image[0]);
     } else if (image[0] && images[oid].derivative) {
       this.switchImageEffect(image, images[oid]);
     } else if (image[0] == null) {
@@ -53,8 +55,8 @@ UCOI.switchImage = function(aid, input, size) {
        parentImage[0].innerHTML = "<img src=\""+images[oid].derivative+"\" class=\"uc-option-image\">";
     }
   }
-  catch (e) {   
-    this.switchImageEffect(image, this.noimage); 
+  catch (e) {
+    this.switchImageEffect(image, this.noimage);
   }
 };
 
@@ -65,13 +67,13 @@ UCOI.switchImageEffect = function(image, imageproperty) {
 
   switch(this.effect){
     case 'fade':
-      $(image).fadeOut(200, function(){		      
+      $(image).fadeOut(200, function(){
         $(this).attr('src', imageproperty.derivative).fadeIn(200);
       });
       break;
-      
+
     default:
-      $(image).attr('src', imageproperty.derivative); 
+      $(image).attr('src', imageproperty.derivative);
   }
 };
 
@@ -80,11 +82,11 @@ UCOI.switchImageEffect = function(image, imageproperty) {
  */
 UCOI.getAID = function(input) {
   var name = $(input).attr('name');
-  return name.match(/attributes\[([0-9]+)\]/)[1]; 
+  return name.match(/attributes\[([0-9]+)\]/)[1];
 };
 
-if (Drupal.jsEnabled) {
-  $(function(){
-    UCOI.init();
-  });
-}
+$(function(){
+  UCOI.init();
+});
+
+})(jQuery);
