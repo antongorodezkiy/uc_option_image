a2eff427e2de50d3d78aa9073df70b47731b68d4
Aaron Craig <me@aaroncraig.com>
2012-06-21 15:21:11 +0200

    Issue #1140634 by aacraig: port to D7

--- uc_option_image.js
+++ uc_option_image.js
@@ -1,92 +1,131 @@
+/**
+ * @file
+ * Show the selected option
+ */
 (function ($) {
+  Drupal.UCOptionImage = Drupal.UCOptionImage || {};
 
-var UCOI = UCOI || {};
+  Drupal.UCOptionImage.selected = [];
 
-/**
- * Initialize.
- */
-UCOI.init = function() {
-  var size = Drupal.settings.UCOI.size;
-  this.images = Drupal.settings.UCOI.images;
-  this.effect = Drupal.settings.UCOI.effect;
-  this.noimage = Drupal.settings.UCOI.noimage;
-  this.attributes = Drupal.settings.UCOI.attributes;
-  this.defaultSize = Drupal.settings.UCOI.default_size;
-  this.nodeid = Drupal.settings.UCOI.nodeid;
+  /**
+   * Initialize the form, and show any images for default selections
+   */
+  Drupal.UCOptionImage.init = function() {
+    Drupal.UCOptionImage.update();
 
-  // Selects
-  $('.add-to-cart select.form-select').change(function(){
-    if (aid = UCOI.getAID(this)){
-      UCOI.switchImage(aid, this, size);
+    $('*[id^=edit-attributes]').bind('change', function() {
+      Drupal.UCOptionImage.update();
+    });
+  };
+
+  /**
+   * Add a selected option
+   */
+  Drupal.UCOptionImage.add_selected = function(aid, oid) {
+    if (!Drupal.UCOptionImage.selected[aid]) {
+      Drupal.UCOptionImage.selected[aid] = [];
     }
-  });
-
-  // Radios
-  $('.add-to-cart .form-radios input').click(function(){
-    if (aid = UCOI.getAID(this)){
-      UCOI.switchImage(aid, this, size);
+    if (!Drupal.UCOptionImage.selected[aid][oid]) {
+      Drupal.UCOptionImage.selected[aid][oid] = true;
     }
-  });
-};
+  };
 
-/**
- * Switch an option image.
- */
-UCOI.switchImage = function(aid, input, size) {
-  var nid = this.nodeid;
-  var oid = $(input).val();
-  var image =  $(':not(.uc-option-image-preloaded) > div.uc-option-image-block').children('img.uc-option-image');
+  /**
+   * Update the selected images, as necessary
+   */
+  Drupal.UCOptionImage.update = function() {
+    Drupal.UCOptionImage.selected = [];
 
-  // Make sure we have permission to switch this attribute
-  if (this.attributes[aid] == 0){
-    return;
+    Drupal.UCOptionImage.update_inputs();
+    Drupal.UCOptionImage.update_selects();
+
+    Drupal.UCOptionImage.show_images();
   }
 
-  try {
-    var images = this.images[nid][aid];
+  /**
+   * Update the image for this attribute if the checkbox / radio is selected
+   */
+  Drupal.UCOptionImage.update_inputs = function(el) {
+    $('input[id^=edit-attributes]:checked').each(function() {
+      var match = $(this).attr('id').match(/edit\-attributes\-(\d+)\-(\d+)/);
+      if (!match) {
+        return;
+      }
 
-    if ((oid=="" || images[oid].derivative=="") && image) {
-	    parentImage = image[0].parentNode;
-	    parentImage.removeChild(image[0]);
-    } else if (image[0] && images[oid].derivative) {
-      this.switchImageEffect(image, images[oid]);
-    } else if (image[0] == null) {
-       parentImage = $(':not(.uc-option-image-preloaded) > div.uc-option-image-block');
-       parentImage[0].innerHTML = "<img src=\""+images[oid].derivative+"\" class=\"uc-option-image\">";
+      var aid = match[1];
+      var oid = match[2];
+
+      Drupal.UCOptionImage.add_selected(aid, oid);
+    });
+  };
+
+  /**
+   * Update the image for this attribute if the select item is chosen
+   */
+  Drupal.UCOptionImage.update_selects = function(el) {
+    $('select[id^=edit-attributes]').each(function() {
+      var match = $(this).attr('id').match(/edit\-attributes\-(\d+)/);
+      if (!match) {
+        return;
+      }
+
+      var aid = match[1];
+      var oid = $(this).val();
+
+      Drupal.UCOptionImage.add_selected(aid, oid);
+    });
+  };
+
+  /**
+   * Display this option image.  If the option is already displayed, it remains visible.
+   *
+   * Multiple choices for the same option will show each choice in the order they are chosen.
+   */
+  Drupal.UCOptionImage.show_images = function() {
+    for (var aid in Drupal.UCOptionImage.selected) {
+      if (!aid) {
+        continue;
+      }
+
+      var el = $('#uc-option-image-selected-' + aid);
+      if (el.length < 1) {
+        continue;
+      }
+
+      el.html('');
+      Drupal.UCOptionImage.show_image(aid);
+    }
+  };
+
+  /**
+   * Add images for a given aid
+   */
+  Drupal.UCOptionImage.show_image = function(aid) {
+    var options = Drupal.settings['uc_option_image-' + aid];
+    if (!options) {
+      return;
+    }
+
+    var el = $('#uc-option-image-selected-' + aid);
+    if (el.length < 1) {
+      return;
+    }
+
+    for (oid in Drupal.UCOptionImage.selected[aid]) {
+      if (!oid) {
+        continue;
+      }
+      if (!options[oid]) {
+        continue;
+      }
+
+      el.append(options[oid]);
     }
   }
-  catch (e) {
-    this.switchImageEffect(image, this.noimage);
+
+  Drupal.behaviors.uc_option_image = {
+    attach: function(context, settings) {
+      Drupal.UCOptionImage.init();
+    }
   }
-};
-
-/**
- * Switch the imagepath based on the selected effect.
- */
-UCOI.switchImageEffect = function(image, imageproperty) {
-
-  switch(this.effect){
-    case 'fade':
-      $(image).fadeOut(200, function(){
-        $(this).attr('src', imageproperty.derivative).fadeIn(200);
-      });
-      break;
-
-    default:
-      $(image).attr('src', imageproperty.derivative);
-  }
-};
-
-/**
- * Get attribute AID from an input.
- */
-UCOI.getAID = function(input) {
-  var name = $(input).attr('name');
-  return name.match(/attributes\[([0-9]+)\]/)[1];
-};
-
-$(function(){
-  UCOI.init();
-});
-
 })(jQuery);
